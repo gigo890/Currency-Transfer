c4291eb1aa4d519b742a68c91f0a340f0f1cd7dd
gigo890 <149149857+gigo890@users.noreply.github.com>
2024-02-24 20:48:04 +0000

    20:47 24/02/2024

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class = "nav">
+        <div class="navlist">
+            <div class="home"><a href="index.php"><img src="images/logo.jpg" alt="College Logo"></a></div>
+            <div id="myLinks">
+                <a href="courses.php">Courses</a>
+                <a href="facilities.php">Facilities</a>
+                <a href="about-us.php">About Us</a>
+            </div>
+        </div>
+        
+        <div class="icon-container">
+            <div href="javascript:void(0);" class="menu" alt="open menu" onclick ="myFunction()">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
